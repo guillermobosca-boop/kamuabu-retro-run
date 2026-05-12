e22635de220c38f8782e77061a5b882bb7030258
guillermobosca-boop <guillermobosca-boop@users.noreply.github.com>
2026-05-12 14:34:14 +0200

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,7 +1,10 @@
-const { sendJson, sendMethodNotAllowed } = require("./_lib/http");
+const { handleOptions, sendJson, sendMethodNotAllowed } = require("./_lib/http");
 const { assertSupabase } = require("./_lib/supabase");
 
 module.exports = async function handler(req, res) {
+  if (handleOptions(req, res)) {
+    return;
+  }
   if (req.method !== "GET") {
     return sendMethodNotAllowed(res, ["GET"]);
   }
