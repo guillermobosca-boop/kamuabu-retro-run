1529aa6efaee3c76af9fbb9b6dd1e0c128bb57ce
guillermobosca-boop <guillermobosca-boop@users.noreply.github.com>
2026-05-12 17:59:29 +0200

    Update global.js

--- api/leaderboard/global.js
+++ api/leaderboard/global.js
@@ -1,5 +1,5 @@
 const { handleOptions, sendJson, sendMethodNotAllowed } = require("../_lib/http");
-const { fetchLeaderboard, normalizeEntries } = require("../_lib/leaderboard");
+const { computeSelfEntry, fetchLeaderboard, normalizeEntries } = require("../_lib/leaderboard");
 
 module.exports = async function handler(req, res) {
   if (handleOptions(req, res)) {
@@ -11,10 +11,12 @@ module.exports = async function handler(req, res) {
 
   try {
     const limit = req.query?.limit || 10;
+    const playerId = String(req.query?.playerId || "");
     const rows = await fetchLeaderboard("global", { limit });
     return sendJson(res, 200, {
       ok: true,
       entries: normalizeEntries(rows),
+      selfEntry: playerId ? await computeSelfEntry("global", { playerId }) : null,
       scope: "global",
       source: "remote",
     });
